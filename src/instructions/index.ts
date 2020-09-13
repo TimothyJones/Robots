c1bff056aec631a8cdf6aff5f9395668aed51714
Timothy Jones <timothy.l.jones@gmail.com>
2020-09-13 20:04:15 +1000

    Improve parser boundary

--- src/instructions/index.ts
+++ src/instructions/index.ts
@@ -1,58 +1,64 @@
-import { InstructionType, PlaceInstruction } from './types';
+import { InstructionType, PlaceInstruction, Instruction } from './types';
 import { RobotFacing, RobotPosition } from '../simulator/state/types';
 
-const instructionsWithNoParameters = [
-  InstructionType.Left,
-  InstructionType.Right,
-  InstructionType.Move,
-  InstructionType.Report,
-];
-
-const parseNoParameterInstruction = (command: string) => {
-  if (instructionsWithNoParameters.includes(command as InstructionType)) {
-    return {
-      type: command as InstructionType,
-    };
-  } else {
-    throw new Error(`${command} is not a valid no-parameter command`);
-  }
-};
-
 const parsePosition = (params: string) => {
-  const A = params.split(',');
-  if (A.length !== 3) {
-    throw new Error(`Position expects 3 arguments, but got ${A.length}`);
+  const tokens = params.split(',');
+  if (tokens.length !== 3) {
+    throw new Error(`Position expects 3 arguments, but got ${tokens.length}`);
   }
-  const x = parseInt(A[0], 10);
-  const y = parseInt(A[1], 10);
-  const facing = A[2];
+  const x = parseInt(tokens[0], 10);
+  const y = parseInt(tokens[1], 10);
+  const facing = tokens[2];
   if (
     isNaN(x) ||
     isNaN(y) ||
     !Object.values(RobotFacing).includes(facing as RobotFacing)
   ) {
-    throw new Error(
-      `Invalid position string '${params}' {${x},${y},${Object.values(
-        RobotFacing
-      )}}`
-    );
+    throw new Error(`Invalid position string '${params}'`);
   }
   return { x, y, facing };
 };
 
+type InstructionGenerator = ((str: string[]) => Instruction) | Instruction;
+
+const instructions: { [idx: string]: InstructionGenerator } = {
+  [InstructionType.Left]: { type: InstructionType.Left },
+  [InstructionType.Right]: { type: InstructionType.Right },
+  [InstructionType.Move]: { type: InstructionType.Move },
+  [InstructionType.Report]: { type: InstructionType.Report },
+  [InstructionType.Place]: (tokens: string[]) => {
+    if (tokens.length !== 2) {
+      throw new Error(
+        "PLACE command takes exactly one argument of the form '<x>,<y>,<facing>' "
+      );
+    }
+    return {
+      type: InstructionType.Place,
+      position: parsePosition(tokens[1]),
+    };
+  },
+};
+
+const generate = (generator: InstructionGenerator, tokens: string[]) => {
+  if (typeof generator === 'undefined') {
+    throw new Error('Unknown command');
+  }
+  if (typeof generator === 'function') {
+    return generator(tokens);
+  }
+  return generator;
+};
+
 export const parseInstruction = (line: string) => {
   try {
-    const A = line.trim().split(' ');
-    if (A.length === 1) {
-      return parseNoParameterInstruction(A[0]);
-    } else if (A.length === 2 && A[0] === InstructionType.Place) {
-      return {
-        type: InstructionType.Place,
-        position: parsePosition(A[1]),
-      } as PlaceInstruction;
-    }
+    const tokens = line
+      .trim()
+      .split(' ')
+      .filter((s) => s !== '');
+
+    return generate(instructions[tokens[0]], tokens);
   } catch (e) {
-    console.warn(`Unknown instruction '${line}': ${e.message}`);
+    console.warn(`Parse error in '${line}': ${e.message}`);
   }
   return { type: InstructionType.NoOp };
 };
